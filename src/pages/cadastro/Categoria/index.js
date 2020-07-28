c2b60f3f1e1467e02940abf86e43b3abb1e9d3fd
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2020-07-28 18:08:39 -0300

    init form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,10 @@ function CadastroCategoria() {
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
 
+            <form>
+                
+            </form>
+
             <Link to="/">
                 Voltar para a home
             </Link>
